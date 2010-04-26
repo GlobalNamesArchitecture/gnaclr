9df022d644926172cfaab3510449413cab66737f
Dmitry Mozzherin <dmozzherin@gmail.com>
2010-04-26 11:57:14 -0400

    2010-04-26 at 11:57:14 AM

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -13,8 +13,10 @@ describe 'main application' do
     last_response.should be_redirect
   end
   
-  it 'should save classification file' do
-    post('/classifications', :multipart => true, :file => File.new(File.join(SiteConfig.root_path, 'spec', 'files', 'data.tar.gz')).read, :agent => 'agent1', :name => 'test', :uuid => UUID.create_v5("one.example.com", UUID::NameSpace_DNS).guid)
+  it 'should save classification file keeping different versions' do
+    post('/classifications', :multipart => true, :file => File.new(File.join(SiteConfig.root_path, 'spec', 'files', 'data_v1.tar.gz')).read, :agent => 'agent1', :name => 'test', :uuid => UUID.create_v5("one.example.com", UUID::NameSpace_DNS).guid)
+    last_response.should be_redirect
+    post('/classifications', :multipart => true, :file => File.new(File.join(SiteConfig.root_path, 'spec', 'files', 'data_v2.tar.gz')).read, :agent => 'agent1', :name => 'test', :uuid => UUID.create_v5("one.example.com", UUID::NameSpace_DNS).guid)
     last_response.should be_redirect
   end
 
