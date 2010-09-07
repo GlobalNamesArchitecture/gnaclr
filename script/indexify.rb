2624753c08938ef351cfbdffea8276d02bc1618f
Dmitry Mozzherin <dmozzherin@gmail.com>
2010-09-06 20:57:03 -0400

    intermediate -- work on ingesting data to solr

--- script/indexify.rb
+++ script/indexify.rb
@@ -42,35 +42,10 @@ Sinatra::Application.set :run, false
 
 require File.join(File.dirname(__FILE__), '..', 'application')
 
-class FastDb 
-  def initialize
-    @db = SQLite3::Database.new( ":memory:" )
-    start
-  end
-
-  def start
-    @db.execute("drop table if exists core")
-    @db.execute("create table core (taxon_id varchar primary key, scientific_name varchar, rank varchar)")  
-    @db.execute("drop table if exists synonyms")
-    @db.execute("create table core (id int primary key autoincrement, taxon_id varchar, scientific_name varchar)")
-    @db.execute("drop table if exists vernacular")
-    @db.execute("create table core (id int primary key autoincrement, taxon_id varchar, vernacular varchar)")  
-  end
-
-  def execute(query)
-    @db.execute query
-  end
-end
-
-
-
-
 classifications = OPTIONS[:classification_id] ? [Classification.first(:id => OPTIONS[:classifications_id])] : Classification.all
 
 classifications.each do |c|
-  puts "%s, %s, %s" % [c.id, c.uuid, c.file_name]
+  # puts "%s, %s, %s" % [c.id, c.uuid, c.file_name]
+  dc = DarwinCore.new(File.join(SiteConfig.files_path, c.uuid,  c.file_name))
+  puts dc.core.fields
 end
-
-db = FastDb.new
-
-puts db.execute("select * from core")
